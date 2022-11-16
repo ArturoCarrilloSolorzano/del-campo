490d077c2d082937d28caf95b3cd1e3cca5b57f9
JonathanMrC <jmcivrian@gmail.com>
2022-11-15 20:24:13 -0600

    style: animation to logo and text

--- components/main_page/hero/index.jsx
+++ components/main_page/hero/index.jsx
@@ -1,14 +1,15 @@
 export default function Hero() {
   return (
       <div className='container h-80 mx-auto  mt-2 shadow-2xl bg-[url("../public/farmland.jpg")] 
-      bg-center bg-no-repeat bg-cover rounded-xl 
+      bg-center bg-no-repeat bg-cover rounded-xl
       max-w-screen-lg flex items-center text-center flex-col'>
         
-        <img src = 'logo.jpg' alt = "logo" className="w-1/6 rounded-bl-lg"/>
+        <img src = 'logo.jpg' alt = "logo" className="w-1/6 rounded-bl-lg hover:w-1/5"/>
 
         <div className="flex items-center w-full flex-auto">
           <div className='bg-gradient-to-br from-green-50 to-green-600 font-medium 
-                          p-0.5 rounded-br-xl rounded-tl-xl w-1/2'> 
+                          p-0.5 rounded-br-xl rounded-tl-xl w-1/2 
+                          hover:animate-bounce'> 
             Acercando a los agricultores <br/>
             al comercio externo
           </div >
