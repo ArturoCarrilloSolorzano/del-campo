b1a5e43fff79b8217515e9d96b3e86c43203141a
JonathanMrC <jmcivrian@gmail.com>
2022-11-16 23:53:13 -0600

    style:add mision and feature feature need more work

--- components/main_page/hero/index.jsx
+++ components/main_page/hero/index.jsx
@@ -1,14 +1,13 @@
 export default function Hero() {
   return (
-      <div className='container h-80 mx-auto  mt-2 shadow-2xl bg-[url("../public/farmland.jpg")] 
-      bg-center bg-no-repeat bg-cover rounded-xl
-      max-w-screen-lg flex items-center text-center flex-col'>
+      <div className='container h-[480px] mx-auto mt-2 shadow-2xl bg-[url("../public/farmland.jpg")] 
+      bg-center bg-no-repeat bg-cover rounded-xl flex items-center text-center flex-col'>
         
-        <img src = 'logo.jpg' alt = "logo" className="w-1/6 rounded-bl-lg hover:w-1/5"/>
+        <img src = 'logo.jpg' alt = "logo" className="sm:w-1/6 w-3/12 rounded-bl-lg hover:w-1/5"/>
 
         <div className="flex items-center w-full flex-auto">
           <div className='bg-gradient-to-br from-green-50 to-green-600 font-medium 
-                          p-0.5 rounded-br-xl rounded-tl-xl w-1/2 
+                          p-0.5 rounded-br-xl rounded-tl-xl w-1/2 sm:text-2xl text-sm
                           hover:animate-bounce'> 
             Acercando a los agricultores <br/>
             al comercio externo
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Hero from "../components/main_page/hero";
+import Mision from "../components/main_page/mision";
+import Feature from "../components/main_page/feature";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Mision />
+      <Feature />
     </div>
   );
 }
