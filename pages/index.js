a03612a1562d1b4491f58ce0da6bb2cce2665fed
JonathanMrC <jmcivrian@gmail.com>
2022-11-20 00:15:21 -0600

    style: add sub section

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Hero from "../components/main_page/hero";
 import Main from "../components/main_page/main";
+import Subscription from "../components/main_page/subscription";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Main />
+      <Subscription />
     </div>
   );
 }
