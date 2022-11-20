f4706672b1fd019adcfe39a72445ba70bb2a82b9
JonathanMrC <jmcivrian@gmail.com>
2022-11-19 23:40:25 -0600

    style: new main style

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Hero from "../components/main_page/hero";
+import Main from "../components/main_page/main";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Main />
     </div>
   );
 }
