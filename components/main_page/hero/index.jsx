476865d29ed81b6ae29a69d3624d7cca84c20ea3
JonathanMrC <jmcivrian@gmail.com>
2022-11-19 22:15:33 -0600

    style: new hero format

--- components/main_page/hero/index.jsx
+++ components/main_page/hero/index.jsx
@@ -1,31 +1,34 @@
 export default function Hero() {
   return (
-      <div className='container h-[480px] mx-auto mt-2 shadow-2xl bg-[url("../public/farmland.jpg")] 
-      bg-center bg-no-repeat bg-cover rounded-xl flex items-center text-center flex-col'>
-        
-        <img src = 'logo.jpg' alt = "logo" className="sm:w-1/6 w-3/12 rounded-bl-lg hover:w-1/5"/>
-
-        <div className="flex items-center w-full flex-auto">
-          <div className='bg-gradient-to-br from-green-50 to-green-600 font-medium 
-                          p-0.5 rounded-br-xl rounded-tl-xl w-1/2 sm:text-2xl text-sm
-                          hover:animate-bounce'> 
-            Acercando a los agricultores <br/>
-            al comercio externo
-          </div >
-
-          <div className="w-1/2 flex justify-around">
-            <button className="bg-orange-500 p-2 rounded text-gray-50  w-30 hover:bg-orange-400"
-                onClick={() => setAgregarOpen(true)}>
-                Ingresar
-            </button>
-            <button className="bg-orange-500 p-2 rounded text-gray-50  w-30 hover:bg-orange-400"
-                onClick={() => setAgregarOpen(true)}>
-                Registrarse
-            </button>
+      <div className="container min-w-full p-10 mx-auto bg-custom-green">
+        <div className="grid grid-cols-2">
+          <div className="grid grid-cols-1 text-white pr-5">
+            <h1 className="font-bold text-center text-3xl">Acercando los agricultores al comercio externo</h1>
+            <p className="font-light text-justify text-xl opacity-80">
+              La principal función de este proyecto es darle poder de elección y negociación
+              a los agricultores frente a los distribuidores de alimentos, así como crear conexiones directas 
+              entre productores y consumidores. 
+            </p>
+            <button className="mt-5 bg-custom-orange sm:w-[100px] lg:w-[200px] max-w-[300px] h-10 rounded-xl">Registrarse</button>
+            <div className="grid sm:grid-cols-1 lg:grid-cols-3 mt-5">
+              <div>
+                <h2>💪</h2>
+                <p>Poder</p>
+              </div>
+              <div>
+                <h2>✋</h2>
+                <p>Elección</p>
+              </div>
+              <div>
+                <h2>👨🏿‍🤝‍👨🏼👩🏼‍🤝‍🧑🏻</h2>
+                <p>Conexiones</p>
+              </div>
+            </div>
+          </div>
+          <div className="pl-5">
+            <img className="rounded-2xl object-cover h-full w-full" src="farmland.jpg" alt = "Imagen del campo"/>
           </div>
         </div>
-        
       </div>
-    
   );
 }
